72230c71eee06b971cfbdf4aa431a324638423b6
Joshua J <joshua.lavallais@yahoo.com>
2022-09-27 22:05:21 -0500

    My first commit

---
+++ resources/index.js
@@ -0,0 +1,24 @@
+//MOBILE MENU BUTTON
+
+//const mobileMenu = document.querySelector('.mobile-button');
+
+//const menuItems = document.querySelector('.drop-down');
+
+
+function toggleMenu() {
+
+    //const mobileMenu = document.querySelector('.mobile-button');
+
+    const menuItems = document.querySelector('.drop-down');
+
+        if(menuItems.style.display === 'none') {
+            menuItems.style.display = 'block';
+                 
+        }
+
+            else {
+                menuItems.style.display = 'none';
+            }
+
+
+}
