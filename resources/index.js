fb3abeaad9cb23682c4fefea579d76aa0417c20e
Joshua J <joshua.lavallais@yahoo.com>
2022-10-10 16:01:21 -0500

    Updated HTML, CSS, and JavaScript files

--- resources/index.js
+++ resources/index.js
@@ -22,3 +22,59 @@ function toggleMenu() {
 
 
 }
+
+const card = document.querySelector('.card-flip');
+
+card.addEventListener('click', () => {
+    card.classList.toggle('flip');
+});
+
+const cardOne = document.querySelector('.card-flip1');
+
+cardOne.addEventListener('click', () => {
+    cardOne.classList.toggle('flip');
+});
+
+const cardTwo = document.querySelector('.card-flip2');
+
+cardTwo.addEventListener('click', () => {
+    cardTwo.classList.toggle('flip');
+});
+
+const cardThree = document.querySelector('.card-flip3');
+
+cardThree.addEventListener('click', () => {
+    cardThree.classList.toggle('flip');
+});
+
+const cardFour = document.querySelector('.card-flip4');
+
+cardFour.addEventListener('click', () => {
+    cardFour.classList.toggle('flip');
+});
+
+const cardFive = document.querySelector('.card-flip5');
+
+cardFive.addEventListener('click', () => {
+    cardFive.classList.toggle('flip');
+});
+
+
+
+//document.querySelectorAll('.card-flip').forEach(n => n.addEventListener('click', () => {
+    //card.classList.toggle('flip');
+//}))
+
+//const arr = document.getElementsByClassname('.card-flips');
+
+//for (const k = 0; k < arr.length; k++)
+   //arr[k].addEventListener('click', () => {
+    //arr.classList.toggle('flip');
+//});
+
+//const cardAll = document.querySelectorAll('.card-flips');
+
+//document.querySelectorAll('.card-flips').forEach(n => n.addEventListener('click', () => {
+    //cardAll.classList.toggle('flip');
+    
+//}))
